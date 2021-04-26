ba185084e39d44ce869aa690a461607a4db3082e
Cristian Gonzalez <cristiangt9@gmail.com>
2021-04-26 10:47:29 -0500

    relations between quotes and comments

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Quote extends Model
 {
-    use HasFactory;
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
